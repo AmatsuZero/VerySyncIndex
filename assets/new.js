cb4fe24a2f4aac3733be874af6e643520facbf8c
AmastuZero <jzh16s@hotmail.com>
2021-11-27 16:30:01 +0800

    [auto] 更新内容

--- assets/new.js
+++ assets/new.js
@@ -1,9 +1,9 @@
 document.writeln(`
+<tr><td>2021/11/27</td><td tooltip="动画片“汪汪队立大功 PAW Patrol”这部美国学龄前儿童教育动画片，也是一部尼克儿童频道2014年热播中的新作，讲述男孩莱德带领的一个狗狗救援队的精彩故事。可以让孩子们在欣赏动画的同时，学习一些基础的安全和救援知识。  家里有小孩的推荐同步哦">【少儿】汪汪队立大功 1-5季全集中文</td><td>42GB</td><td>B45MLVNKO2BDJ7B35IB26OWAXOGOVLPL7X2VNMCXL4WBYGHYRCYHRQ</td></tr>
 <tr><td>2021/11/26</td><td tooltip="良医第四季是美国2020上映的剧情电视剧,弗莱迪·海默主演的医疗剧集《良医》发布续订第4季。">【电视剧】美剧《良医》第四季</td><td>41.1GB</td><td>B4ZCGVTVV4H5FMQCPUYJZXNIZP7A7SBAYBHYR4IMAWD5GM7EXQ3EEA</td></tr>
 <tr><td>2021/11/26</td><td tooltip="电影合集持续更新。">【电影】电影合集（持续更新）108p和4K画质</td><td>18GB</td><td>B43M32JSIW7USG5XOFBQ6BO5VVMK4ZVI5BREMQKD7BPPXDE47IJQHQ</td></tr>
 <tr><td>2021/11/25</td><td tooltip="缘分の天空  113G典藏资源。。适合收藏。配套HiRes专辑">【动漫】缘分の天空 yzk蓝光圆盘典藏资源+[HiRes]金标音源</td><td>113GB</td><td>B3APF6B5G2EW55RKUFMYLLMH67JX66HOU23C5LQOBXR5MYYA7XQ4VQ6</td></tr>
 <tr><td>2021/11/25</td><td tooltip="黑寡妇HD1080p">【电影】黑寡妇</td><td>5.77GB</td><td>B4BIUEUK4PQE4NNCFEWSKTSJ4MTCW6J6SMROOPV6655AY74HLWTR4A</td></tr>
 <tr><td>2021/11/23</td><td tooltip="一人之下1到4季（第4季更新中）">【动漫】一人之下1到4季（第4季更新中）</td><td>22GB</td><td>B4TLRD5MIVDKSI5NBEGW6CE2BDBZHFCR7B4ZB2ULY73GKQZW4UPGDA</td></tr>
 <tr><td>2021/11/23</td><td tooltip="暴力摩托">【娱乐软件】暴力摩托</td><td>331MB</td><td>B4BH35VRVJXOHERPXJYF3HRS7UYMP6TPWHFKGCLTGPJCJP4AFYIOTA</td></tr>
-<tr><td>2021/11/22</td><td tooltip="收集自网络mame街机共8628个游戏">【娱乐软件】街机集合</td><td>17.5GB</td><td>B4ACQS3H2K6F7KU7YNCWCCKBWEIFBRPYHM2RUBEGWG2B7BLCLYACAA</td></tr>
 `)
--- inc/se.js
+++ inc/se.js
@@ -1,4 +1,5 @@
 document.writeln(`
+<tr><td tooltip="动画片“汪汪队立大功 PAW Patrol”这部美国学龄前儿童教育动画片，也是一部尼克儿童频道2014年热播中的新作，讲述男孩莱德带领的一个狗狗救援队的精彩故事。可以让孩子们在欣赏动画的同时，学习一些基础的安全和救援知识。  家里有小孩的推荐同步哦">汪汪队立大功 1-5季全集中文</td><td>42GB</td><td class="se_k">B45MLVNKO2BDJ7B35IB26OWAXOGOVLPL7X2VNMCXL4WBYGHYRCYHRQ</td></tr>
 <tr><td tooltip="故事讲述了飞机机器人乐迪与一群称为“超级飞侠”的小伙伴环游世界，为小朋友递送包裹，并帮助他们解决困难的故事。作品最大的特点是每一集发生的城市都不同，用精美的画面展现了世界多元文化，例如名胜、建筑、语言、节日和美食等。故事传递着善良、勇敢、欢乐和友爱，让孩子跟随乐迪在每一次新的神奇之旅中，可以开阔眼界并认识到多彩绚丽的地球家园。">超级飞侠1-9季合集</td><td>34.3GB</td><td class="se_k">B4O6ONR2XNVCQ34NYIU2NKBXLQDWMFA3WP3C6DUTPJCJTDC2GXC33A</td></tr>
 <tr><td tooltip="1998年动画片">西游记 动画版</td><td>15.5GB</td><td class="se_k">B4HC4FSJRXTGKTX2X4WF5SLK4OA6ERECDEPMYNQ7F6WW7IBG6XVV2Q</td></tr>
 <tr><td tooltip="小猪佩奇第一季全集">小猪佩奇全集</td><td>5.52GB</td><td class="se_k">B4NYVMDJKGE47GXQSKZZXVYPMIQ365ZH2AMZ4O52QL64FWSNQ3GLOA</td></tr>
