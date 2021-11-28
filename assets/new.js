a8b8e7fd7502531ed0cf511ba462dc3b4c9f6013
AmastuZero <jzh16s@hotmail.com>
2021-11-29 01:30:01 +0800

    [auto] 更新内容

--- assets/new.js
+++ assets/new.js
@@ -1,9 +1,9 @@
 document.writeln(`
+<tr><td>2021/11/29</td><td tooltip="美剧合集（持续更新）现在更新了洛基第一季。">【电视剧】美剧合集（持续更新）</td><td>6.93GB</td><td>B4ERMTWDPLJPC37THYZD7OV5EOHATFLS2QXFFDPSYGZ6MIQB2ID2CQ</td></tr>
+<tr><td>2021/11/29</td><td tooltip="Hi-Res高解析音源  热点资源。。霉霉10套专辑 ，久石让（久石让的一堆），坂本龙一，八爷（米津玄师。大概也有6套专辑），加州旅馆（加州招待所HIFI必听）， 夜愿，核爆，某科学超电磁炮，水桥舞2套，A神，泽野弘之，亚当兰伯特，滨崎步，reol，仓木麻衣，宇多田光，推荐手机同步。喜欢哪个同步哪个">【日韩音乐】[Hi-Res]高解析音源</td><td>226GB</td><td>B3AK6W75QHQ2PJW2CZ3JPHGSI7AR645GA2FD33NKQZQ5SHUI35XBLQG</td></tr>
 <tr><td>2021/11/29</td><td tooltip="【动漫】经典动漫（长篇，完结，经典等）（长期更新）">【动漫】【动漫】经典动漫（长篇，完结，经典等）（长期更新）</td><td>624GB</td><td>B4QEA5ZPJZHVJBOLVCT57FD45UX3STD44JY6ISZU2CRVMHYMAL72PA</td></tr>
 <tr><td>2021/11/29</td><td tooltip="电影合集（持续更新）">【电影】电影合集（持续更新）</td><td>133GB</td><td>B43M32JSIW7USG5XOFBQ6BO5VVMK4ZVI5BREMQKD7BPPXDE47IJQHQ</td></tr>
 <tr><td>2021/11/29</td><td tooltip="动漫新番+剧场版（持续更新）长期更新">【动漫】动漫新番+剧场版（持续更新）</td><td>380GB</td><td>B3AN6QTDEXM4TZZWUTIMSIQ5JAYFXTQ2YAFJX3BBI7OAQDBGYYISHXI</td></tr>
 <tr><td>2021/11/29</td><td tooltip="童年的回忆">【娱乐软件】街机合集（经典街机+小霸王+口袋妖怪+三国战纪西游记等）可用键盘手柄</td><td>3.76GB</td><td>B3AMPZQNCXAXFF7KHUNPAJ2TVRHX4ZAAMNE3AXPFA2UH57VRZ33DDUM</td></tr>
 <tr><td>2021/11/29</td><td tooltip="人民的名义4k60帧源">【电视剧】人民的名义4k60帧源</td><td>119GB</td><td>B3AKTL6SMX4I2ILPKUAZKQM544GWKSK4ALG37OT7HHAIGTYEDLCWNUM</td></tr>
-<tr><td>2021/11/29</td><td tooltip="宫崎骏28部动画作品终极收藏版1080P">【动漫】宫崎骏28部动画作品终极收藏版1080P</td><td>129GB</td><td>B4VM3CS4GJLUIC36KXA2LHVMC5MF7RXS7ALL56RM74OGHJQ7WFX5ZQ</td></tr>
-<tr><td>2021/11/27</td><td tooltip="动画片“汪汪队立大功 PAW Patrol”这部美国学龄前儿童教育动画片，也是一部尼克儿童频道2014年热播中的新作，讲述男孩莱德带领的一个狗狗救援队的精彩故事。可以让孩子们在欣赏动画的同时，学习一些基础的安全和救援知识。  家里有小孩的推荐同步哦">【少儿】汪汪队立大功 1-5季全集中文</td><td>42GB</td><td>B45MLVNKO2BDJ7B35IB26OWAXOGOVLPL7X2VNMCXL4WBYGHYRCYHRQ</td></tr>
 `)
