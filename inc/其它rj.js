8a99fe902e2436209be6a54c5ebc724a83e6bc74
AmastuZero <jzh16s@hotmail.com>
2021-11-29 19:30:01 +0800

    [auto] 更新内容

---
+++ inc/其它rj.js
@@ -0,0 +1,2 @@
+document.writeln(`
+<tr><td tooltip="Beyond Compare是一套由Scooter Software开发的软件。 它的主要目的是可以能够比较以下三种：  电脑上两个目录的内容，  电脑计算机上的目录和ftp地址的内容  两个文本文件程序的内容（包括UTF-8， html和delphi源程序等文本文件）。  由于这些功能， Beyond Compare用于版本控制和数据信息同步。">Beyond Compare 4.4.0 破解版</td><td>67MB</td><td class="其它rj_k">A4ZJZWDMZDW7PP6G36DFUKO3SWPIJBY2RW2I3JWDGUDY53JAO77G5A</td></tr>
