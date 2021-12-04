a7f343e3cfc9b798fda474582f89cc36468fecd9
AmastuZero <jzh16s@hotmail.com>
2021-12-04 22:30:01 +0800

    [auto] 更新内容

--- assets/new.js
+++ assets/new.js
@@ -1,7 +1,7 @@
 document.writeln(`
+<tr><td>2021/12/04</td><td tooltip="ベートーヴェン交響曲全集久石让 - 贝多芬交响曲全集 Beethoven Complete Symphonies Joe Hisaishi 2019 [DSD256 DSF]">【欧美音乐】久石让 - 贝多芬交响曲全集 [DSF 分轨]</td><td>51.3GB</td><td>B45O2Y7KSHXD7OZDOONUUA6JMOJX2LXC3PUKFGSGHXI5UHWVC7OA6Q</td></tr>
 <tr><td>2021/11/30</td><td tooltip="动漫新番 剧场版（持续更新）长期更新。 2021/11/30   已经更新">【动漫】动漫新番 剧场版（持续更新）</td><td>387GB</td><td>B3AN6QTDEXM4TZZWUTIMSIQ5JAYFXTQ2YAFJX3BBI7OAQDBGYYISHXI</td></tr>
 <tr><td>2021/11/30</td><td tooltip="爱死机第一季1080P">【电影】爱死机第一季1080P</td><td>8.35GB</td><td>B3AKCYQUYCEENMBRNZSXDHY6HVIQUVDBBBFQA2XB4BL3OAGHDYJXQXA</td></tr>
 <tr><td>2021/11/29</td><td tooltip="现在更新了鹰眼第一季（1-2集）">【电视剧】美剧合集（持续更新）</td><td>9.9GB</td><td>B4ERMTWDPLJPC37THYZD7OV5EOHATFLS2QXFFDPSYGZ6MIQB2ID2CQ</td></tr>
 <tr><td>2021/11/29</td><td tooltip="Hi-Res高解析音源  热点资源。。霉霉10套专辑 ，久石让（久石让的一堆），坂本龙一，八爷（米津玄师。大概也有6套专辑），加州旅馆（加州招待所HIFI必听）， 夜愿，核爆，某科学超电磁炮，水桥舞2套，A神，泽野弘之，亚当兰伯特，滨崎步，reol，仓木麻衣，宇多田光，推荐手机同步。喜欢哪个同步哪个">【日韩音乐】[Hi-Res]高解析音源</td><td>226GB</td><td>B3AK6W75QHQ2PJW2CZ3JPHGSI7AR645GA2FD33NKQZQ5SHUI35XBLQG</td></tr>
-<tr><td>2021/11/29</td><td tooltip="【动漫】经典动漫（长篇，完结，经典等）（长期更新）">【动漫】【动漫】经典动漫（长篇，完结，经典等）（长期更新）</td><td>624GB</td><td>B4QEA5ZPJZHVJBOLVCT57FD45UX3STD44JY6ISZU2CRVMHYMAL72PA</td></tr>
 `)
--- inc/omyy.js
+++ inc/omyy.js
@@ -1,4 +1,5 @@
 document.writeln(`
+<tr><td tooltip="ベートーヴェン交響曲全集久石让 - 贝多芬交响曲全集 Beethoven Complete Symphonies Joe Hisaishi 2019 [DSD256 DSF]">久石让 - 贝多芬交响曲全集 [DSF 分轨]</td><td>51.3GB</td><td class="omyy_k">B45O2Y7KSHXD7OZDOONUUA6JMOJX2LXC3PUKFGSGHXI5UHWVC7OA6Q</td></tr>
 <tr><td tooltip="霉霉11月12新专辑">[Hires[]霉霉11月12日发布的新专辑Red (Taylor&#39;s Version)</td><td>4GB</td><td class="omyy_k">B44YNGBQNHFYWKV77XFC5DOX26J4TU6N4YL4EFIPZYSVLUZVWCIDYQ</td></tr>
 <tr><td>前苏联音乐</td><td>1.05GB</td><td class="omyy_k">B23WA6XHUFREVNGK7KWP47BJH3UW3RJWJ7FX25R5RKZPQPBLHOU3Q5BIQB</td></tr>
 `)
