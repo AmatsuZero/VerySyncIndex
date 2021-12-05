e305429b13a13ba382f1e34e04d94c416ac8669c
AmastuZero <jzh16s@hotmail.com>
2021-12-05 17:30:01 +0800

    [auto] 更新内容

--- assets/new.js
+++ assets/new.js
@@ -1,3 +1,2 @@
 document.writeln(`
-<tr><td>2021/12/05</td><td tooltip="曾经的mebook我的小书屋全站电子书，删除了部分同书籍格式不同的其余格式，只保留epub或mobi格式优先 本次整理版本为v0.1 ，还剩一大半没有整理，但本次没时间了">【出版书】mebook全站电子书</td><td>134GB</td><td>B4JSJXCAC44VO2NFXTRA73J4CB6KCPHZLJ6PGKMFZB2AKE34UAG4DQ</td></tr>
-`)
+<tr><td>2021/12/05</td><td tooltip="曾经的mebook我的小书屋全站电子书，删除了部分同书籍格式不同的其余格式，只保留epub或mobi格式优先，书籍按类别分类，本次整理版本为v0.1 ，还剩很多没有整理，剩下的有时间再整理">【出版书】mebook全站电子书</td><td>130GB</td><td>B4JSJXCAC44VO2NFXTRA73J4CB6KCPHZLJ6PGKMFZB2AKE34UAG4DQ</td></tr>
