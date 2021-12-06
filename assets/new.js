aab232090aa8c3a995bd70208403196aa4a9843b
AmastuZero <jzh16s@hotmail.com>
2021-12-07 07:30:01 +0800

    [auto] 更新内容

--- assets/new.js
+++ assets/new.js
@@ -1,1 +1,2 @@
 document.writeln(`
+<tr><td>2021/12/07</td><td tooltip="坂井泉水161首MP3">【日韩音乐】坂井泉水161首MP3</td><td>2250MB</td><td>B4OPWKMSUN6I75GVZ4DJ63MU6ULZMX6EPPRQ5ZQNFSNIR233PAKX5A</td></tr>
--- inc/rhyy.js
+++ inc/rhyy.js
@@ -1,4 +1,5 @@
 document.writeln(`
+<tr><td tooltip="坂井泉水161首MP3">坂井泉水161首MP3</td><td>2250MB</td><td class="rhyy_k">B4OPWKMSUN6I75GVZ4DJ63MU6ULZMX6EPPRQ5ZQNFSNIR233PAKX5A</td></tr>
 <tr><td tooltip="Hi-Res高解析音源  热点资源。。霉霉10套专辑 ，久石让（久石让的一堆），坂本龙一，八爷（米津玄师。大概也有6套专辑），加州旅馆（加州招待所HIFI必听）， 夜愿，核爆，某科学超电磁炮，水桥舞2套，A神，泽野弘之，亚当兰伯特，滨崎步，reol，仓木麻衣，宇多田光，推荐手机同步。喜欢哪个同步哪个">[Hi-Res]高解析音源</td><td>226GB</td><td class="rhyy_k">B3AK6W75QHQ2PJW2CZ3JPHGSI7AR645GA2FD33NKQZQ5SHUI35XBLQG</td></tr>
 <tr><td tooltip="合集来源由三部分组成。一是verycd上5.6G版的吉卜力动画音乐合集，囊括了从《风之谷》到《地海传说》的吉卜力动画音乐，以及相关的精选集；二是新近上映的吉卜力作品音乐，包括《悬崖上的金鱼姬》和《借东西的小人阿丽埃蒂》；三是2008年《久石让in武道馆——与宫崎骏动画一同走过的25年》音乐会的MP3版本，是在久石让音乐论坛下载的网友自压板。所有专辑均为320K的MP3，应该是目前所能找到的最全的版本了。">宫崎骏动画音乐合集56CD</td><td>6.61GB</td><td class="rhyy_k">B4PRUTMGPBJXBNN6W6AS3JOBCRSAH4ARFTO2BIRKJBRRKGQGXDFCWA</td></tr>
 <tr><td tooltip="无损音乐flac格式，有动漫音乐，也有经典音乐，对日语音乐感兴趣的可以瞧瞧，继续更新的">无损日语音乐</td><td>9.65GB</td><td class="rhyy_k">B3ANSWAUK6ILOTL3HZ5QEOR5H4QL3NABVLOVAXAX6T6VD6KJSQDTDWI</td></tr>
