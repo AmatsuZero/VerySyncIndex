965a56fcfb1b8251befff5509d3d0a4039165057
AmastuZero <jzh16s@hotmail.com>
2021-12-11 22:30:01 +0800

    [auto] 更新内容

--- inc/se.js
+++ inc/se.js
@@ -1,6 +1,6 @@
 document.writeln(`
+<tr><td tooltip="小猪佩奇是一个可爱的但是有些小专横的小猪。她已经五岁了，与她的猪妈妈，猪爸爸，和弟弟乔治生活在一起。佩奇最喜欢做的事情是玩游戏，打扮的漂漂亮亮，渡假，以及在小泥坑里快乐的跳上跳下和与苏西羊（她最好的朋友）乔治（她的弟弟）一起玩儿，拜访她的猪爷爷，猪奶奶">小猪佩奇 第1-8季 国语</td><td>16.2GB</td><td class="se_k">B4V5W4D3A25ZNMYVSKHHRAJORVWFEFUOMRKFBCSIRCZW5ISJOXYBRQ</td></tr>
 <tr><td tooltip="动画片“汪汪队立大功 PAW Patrol”这部美国学龄前儿童教育动画片，也是一部尼克儿童频道2014年热播中的新作，讲述男孩莱德带领的一个狗狗救援队的精彩故事。可以让孩子们在欣赏动画的同时，学习一些基础的安全和救援知识。  家里有小孩的推荐同步哦">汪汪队立大功 1-5季全集中文</td><td>42GB</td><td class="se_k">B45MLVNKO2BDJ7B35IB26OWAXOGOVLPL7X2VNMCXL4WBYGHYRCYHRQ</td></tr>
 <tr><td tooltip="故事讲述了飞机机器人乐迪与一群称为“超级飞侠”的小伙伴环游世界，为小朋友递送包裹，并帮助他们解决困难的故事。作品最大的特点是每一集发生的城市都不同，用精美的画面展现了世界多元文化，例如名胜、建筑、语言、节日和美食等。故事传递着善良、勇敢、欢乐和友爱，让孩子跟随乐迪在每一次新的神奇之旅中，可以开阔眼界并认识到多彩绚丽的地球家园。">超级飞侠1-9季合集</td><td>34.3GB</td><td class="se_k">B4O6ONR2XNVCQ34NYIU2NKBXLQDWMFA3WP3C6DUTPJCJTDC2GXC33A</td></tr>
 <tr><td tooltip="1998年动画片">西游记 动画版</td><td>15.5GB</td><td class="se_k">B4HC4FSJRXTGKTX2X4WF5SLK4OA6ERECDEPMYNQ7F6WW7IBG6XVV2Q</td></tr>
-<tr><td tooltip="小猪佩奇第一季全集">小猪佩奇全集</td><td>5.52GB</td><td class="se_k">B4NYVMDJKGE47GXQSKZZXVYPMIQ365ZH2AMZ4O52QL64FWSNQ3GLOA</td></tr>
 `)
