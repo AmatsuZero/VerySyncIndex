32c1b29964e5818c86808b1502ccc65473051a2a
AmastuZero <jzh16s@hotmail.com>
2021-12-11 20:30:02 +0800

    [auto] 更新内容

--- assets/new.js
+++ assets/new.js
@@ -1,2 +1,3 @@
 document.writeln(`
 <tr><td>2021/12/11</td><td tooltip="视频教程（含素材）">【知识培训】Photoshop 2020 入门到精通</td><td>1.4GB</td><td>B4QNN6Y5VZ4HZIPFYRWXNKGAM2YN7HTXXCIEJI4YLFKVDMF5AQHGIA</td></tr>
+`)
