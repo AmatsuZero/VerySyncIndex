5a860825e822f3e6506cca1f6c3be9aa262d33e0
AmastuZero <jzh16s@hotmail.com>
2021-12-22 21:30:01 +0800

    [auto] 更新内容

--- inc/dy.js
+++ inc/dy.js
@@ -1,4 +1,5 @@
 document.writeln(`
+<tr><td tooltip="清晰度：1080P 字幕：英语、西班牙语">HBO Max-黑客帝国：矩阵重启</td><td>9.28GB</td><td class="dy_k">B4T4B5JXXFSG64X525IU55JNC3SK2GO57SGXGVJUHZ35ZSFKXFOWOA</td></tr>
 <tr><td tooltip="个人喜欢的电影，随看随录入。">热门电影（实时更新）</td><td>300GB</td><td class="dy_k">B4XXC2WYNRLIDZYPX4QBMUIIB5HUB7T5W5E3DYLCQZCSDKDY6EP3HQ</td></tr>
 <tr><td tooltip="4k原生60帧，双子杀手">4k60帧双子杀手 原生60帧</td><td>11GB</td><td class="dy_k">B3AIG6SBFF4KDNWTEAUFPL5YYCSJ7AY4TJYUD3NRELGXPFM5V4HMROW</td></tr>
 <tr><td tooltip="《黑镜第五季》是由美国NetFlix公司出品，詹姆斯·哈维斯执导，安德鲁·斯科特、 麦莉·赛勒斯、安东尼·麦凯等人主演的反映近未来黑科技的科幻剧。 该剧依旧延续剧集的暗黑讽刺风格，将多个全新的故事剪辑在一起，以颇具科幻感的未来世界，讲述极具现实感的故事。">黑镜.S05全集.WEB.HEVC.1080P.特效字幕</td><td>8GB</td><td class="dy_k">A4ZRP7ABYJ2SO2QNISO3RBHSZFYIX7F3376VWD6RIEEPNGYZG6454Q</td></tr>
