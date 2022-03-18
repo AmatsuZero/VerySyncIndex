3defb6a05ffe735dc25e22536fc5f863699a8185
AmatsuZero <jzh16s@hotmail.com>
2022-03-18 12:30:01 +0800

    [auto] 更新内容

--- inc/zyp.js
+++
@@ -1,7 +0,0 @@
-document.writeln(`
-<tr><td tooltip="由广为流传的35M 50fps MP2双声道源录制,不知道是央视临时调整了播出时间，还是被EPG信息坑了,另外一个30fps 25M AC-3 5.1声道的HLG源录制不完整，还不能回看就很尴尬,看后续4K频道还会不会重新播出吧">【CCTV4K】2021年戏曲春晚（看说明）</td><td>47.8GB</td><td class="zyp_k">B3AO7FGIU3H3BWK3RH5JKGT3WIOSOXZ4NDEKNTFBALSJYIFAPTB7U7K</td></tr>
-<tr><td tooltip="General Complete name: 2021.CCTV4K.Spring.Festival.Gala.Evening.2160P.UHDTV.HEVC.HLG.DD.5.1.mp4">【CCTV4K】2021年春晚</td><td>52GB</td><td class="zyp_k">B3ALDAC6KCVODA4H6EQBF645YQGFAKO4U4NVJOPTEXPP5KFOV37ONB4</td></tr>
-<tr><td tooltip="周杰伦 -《2007巡回演唱会(台湾版)》-NAS在线，一定让你下完。">周杰伦 -《2007巡回演唱会(台湾版)》</td><td>1.4GB</td><td class="zyp_k">B3ANK7G5VOP6MSEFX4RMIMVPQCVMKDVGO77BF7EPQJ4B6KN3VIPEV36</td></tr>
-<tr><td>小鵬奇啪行</td><td>3.21GB</td><td class="zyp_k">B3ANAMRMYVWN7USCTJWTYTE46Q2QPSKOMCZBRQS6MDIKLIXIXKWVWZK</td></tr>
-<tr><td>[维多利亚的秘密内衣秀2003~2017合集]超清</td><td>11G</td><td class="zyp_k">B3AK4YP74MIUL5OLXV5U7O5FEPBHTC2ZWSCCENLEQ23SPBYRMQ5427K</td></tr>
-`)
