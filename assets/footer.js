2466de59a3f0ade8e3927fca3dd67ed24cf40a78
AmatsuZero <jzh16s@hotmail.com>
2022-03-27 12:30:01 +0800

    [auto] 更新内容

--- assets/footer.js
+++
@@ -1,5 +0,0 @@
-document.writeln(`
-<br />
-<p class="center">Power by <a target="_blank" href="http://www.verysync.com/">VerySync</a></p>
-<p class="center"><a target="_blank" href="assets/about.html">关于神Key</a></p>
-`)
--- inc/qtrj.js
+++
@@ -1,3 +0,0 @@
-document.writeln(`
-<tr><td>设计师福利，付费字体大合集</td><td>800MB</td><td class="qtrj_k">B3AJD6BWLTAWFHXBOMQSKWKDKEM4XDPYVEL6BEN3IGDD3UIDCQSVNQW</td></tr>
-`)
